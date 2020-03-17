885a200264c49d1f0a388c916c7092636819d581
atvho <atvho@i.ua>
2020-03-17 13:49:20 +0200

    Deleted basic.js and added waitUntil method

--- specs/staff_login.js
+++ specs/staff_login.js
@@ -10,10 +10,20 @@ describe('Login with credentials', function(){
         const emailField = $('input[type="email"]');
         const passwordField = $('input[type="password"]');
         const submit = $('button[type="submit"]');
+            browser.waitUntil(function(){
+                return emailField.isDisplayed()
+            }, 7000, 'email field is not rendered yet');
         emailField.addValue(email);
+            browser.waitUntil(function(){
+                return passwordField.isDisplayed()
+            }, 7000, 'password field is not rendered yet')
         passwordField.addValue(password);
         submit.click();
-        browser.pause(4000);
+        // browser.pause(4000);
+        const dashboardMenu = $('button[aria-label="Dashboard"] > span');
+            browser.waitUntil(function(){
+                return dashboardMenu.isDisplayed()
+            }, 7000, 'Dashoard menu is not rendered')
         const currentUrl = browser.getUrl();
         assert.strictEqual(currentUrl, 'https://app.staff.com/#/dashboard');
         
